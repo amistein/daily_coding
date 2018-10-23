d602a79c16c65209806b366f11748f7b720656d8
Amrom Steinmetz <amroms@gmail.com>
2018-10-23 15:18:25 -0400

    P05: add

---
+++ P05.js
@@ -0,0 +1,20 @@
+/*
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+Given this implementation of cons:
+
+function cons(a, b) {
+  function pair(f){
+    return f(a, b)
+  }      
+  return pair
+}
+
+Implement car and cdr.
+*/
+
+const car = p => p(e => e)
+const cdr = p => p((_, e) => e)
+
+car(cons(3,4)) // 3
+cdr(cons(3,4)) // 4
