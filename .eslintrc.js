82f5bcb18c62646c81b1d05b0f8166beebf4473f
Amrom Steinmetz <amroms@gmail.com>
2018-10-23 15:50:23 -0400

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "multiline-comment-style": [
+            "error",
+            "starred-block"
+        ]
+    }
+};
