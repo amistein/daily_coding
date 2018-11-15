af8aade0a7111efea45babc3e9ce3626aaaa19cc
Amrom Steinmetz <amroms@gmail.com>
2018-11-15 18:54:18 -0500

    async.js: add comment

--- async.js
+++ async.js
@@ -1,3 +1,20 @@
+/*
+ * Function to mimic the behavior of the Javascript `async`
+ * keyword using generators. `async/await` uses generators
+ * and the `yield` keywork under the hood.
+ */
+
+function aSync(it) {
+  function iterate(value) {
+    const c = it.next(value)
+    if (c.done) {
+      return Promise.resolve(c.value)
+    }
+    return c.value.then(iterate)
+  }
+  return iterate()
+}
+
 function afterTwoSec(str) {
   return new Promise(r => {
     setTimeout(() => r(str), 2000)
@@ -10,16 +27,6 @@ function* getName() {
   return `${first} ${last}`
 }
 
-const aSync = it => {
-  function iterate(value) {
-    const c = it.next(value)
-    if (c.done) {
-      return Promise.resolve(c.value)
-    }
-    return c.value.then(iterate)
-  }
-  return iterate()
-}
 
 // eslint-disable-next-line no-console
 aSync(getName()).then(n => console.log(n))
