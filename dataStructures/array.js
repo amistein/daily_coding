62b4b4cd8d562373885429448f7a4a22ca255742
Amrom Steinmetz <amroms@gmail.com>
2018-11-18 15:19:45 -0500

    add array ds file

---
+++ dataStructures/array.js
@@ -0,0 +1,12 @@
+// Kadane's Algorithm
+// Largest Sum Contiguous Subarray
+function maxCont(arr) {
+  function loop(arr, currMax, runningMax) {
+    if (!arr.length) return runningMax
+    const [first, second] = arr
+    if (second === first + 1) return loop(arr.slice(1), currMax + first, runningMax)
+    return loop(arr.slice(1), 0, Math.max(runningMax, currMax + first))
+  }
+
+  return loop(arr, 0, -Infinity)
+}
