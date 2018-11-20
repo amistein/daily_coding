6983992ca8b8c20edc0b14ad00f17175e8f4f34c
Amrom Steinmetz <amroms@gmail.com>
2018-11-20 18:58:55 -0500

    array: add minPlatforms

--- dataStructures/array.js
+++ dataStructures/array.js
@@ -55,5 +55,23 @@ function shortestPathSum(arr) {
   return next ? current.map((n, i) => n + Math.min(next[i], next[i + 1])) : current
 }
 
-const arr = [[1], [7,3], [4,36,2], [8,11,6,12]]
-shortestPathSum(arr)
+shortestPathSum([[1], [7,3], [4,36,2], [8,11,6,12]])
+
+
+function minPlatforms(arrivals, departures) {
+  function loop(events, max, current) {
+    if (!events.length) return max
+
+    const [event] = events
+    const newCurrent = event.type === "a" ? current + 1 : current - 1
+    return loop(events.slice(1), Math.max(max, newCurrent), newCurrent)
+  }
+
+  const arrivalsObjs = arrivals.map(t => ({time: t, type: "a"}))
+  const departObjs = departures.map(t => ({time: t, type: "d"}))
+  const sorted = [...arrivalsObjs, ...departObjs].sort((a, b) => a.time - b.time)
+
+  return loop(sorted, 0, 0)
+}
+
+minPlatforms([900,  940, 950,  1100, 1500, 1800], [910, 1200, 1120, 1130, 1900, 2000])
