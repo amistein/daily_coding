4c4ccdf744452a8e0e33695bcb2a27b37e6117dd
Amrom Steinmetz <amroms@gmail.com>
2018-11-20 09:53:15 -0500

    array: add lis

--- dataStructures/array.js
+++ dataStructures/array.js
@@ -1,5 +1,7 @@
-// Kadane's Algorithm
-// Largest Sum Contiguous Subarray
+/*
+ * Kadane's Algorithm
+ * Largest Sum Contiguous Subarray
+ */
 function maxCont(arr) {
   function loop(arr, currMax, runningMax) {
     if (!arr.length) return runningMax
@@ -9,4 +11,16 @@ function maxCont(arr) {
   }
 
   return loop(arr, 0, -Infinity)
+}
+
+// Longest Increasing Subsequence
+function lis(arr) {
+  function longestIncreasingToIndex(accArr) {
+    if (accArr.length === arr.length) return accArr
+    const currentIndex = accArr.length
+    const filtered = accArr.filter((_, i) => arr[i] < arr[currentIndex])
+    return longestIncreasingToIndex(accArr.concat(1 + (!filtered.length ? 0 : Math.max(...filtered))))
+  }
+
+  return Math.max(...longestIncreasingToIndex([]))
 }
