ca16afd28cc9f86d23784d00a317067e6dcb47f0
Amrom Steinmetz <amroms@gmail.com>
2018-11-28 22:41:17 -0500

    algos: add minJump

--- algos/dynamicProgramming.js
+++ algos/dynamicProgramming.js
@@ -14,4 +14,25 @@ function knapsack(max, vals, weights) {
   )
 }
 
+function minJumps(arr) {
+  const head = arr[0]
+  if (head === 0) return Infinity
+  if (!head) return 0
+
+  const allJumps = Array(head).fill().map((_, i) => minJumps(arr.slice(i + 1)))
+  const min = Math.min(...allJumps)
+  return min === Infinity ? -1 : min + 1
+}
+
+function minJumpsDynamic(arr) {
+  const minJumpsArr = arr.reverse().reduce((result, n) => {
+    const allNexts = result.slice(0, n)
+    const min = Math.min(...allNexts)
+    return [result.length < n ? 1 : 1 + min].concat(result)
+  }, [])
+
+  return minJumpsArr.shift()
+}
+
 knapsack(7, [1,4,5,7], [1,3,4,5])
+minJumpsDynamic([1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9])
