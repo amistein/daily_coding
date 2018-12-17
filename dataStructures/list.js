3e72174740ba39084e382f3203473a10b779ed86
Amrom Steinmetz <amroms@gmail.com>
2018-12-17 10:27:06 -0500

    linked list: add reverse

--- dataStructures/list.js
+++ dataStructures/list.js
@@ -9,15 +9,36 @@ class List {
     return this.tail.get(index - 1)
   }
 
+  size() {
+    if (!this.tail) return 1
+    return 1 + this.tail.size()
+  }
+
+  copy() {
+    const c = new List(this.val)
+    c.tail = this.tail
+    return c
+  }
+
+  drop(index) {
+    if (index === 0) return this
+    return this.tail.drop(index - 1)
+  }
+
+  take(index) {
+    if (!this.tail || index === 0) return new List(this.val)
+    return this.tail.take(index - 1).prepend(this.val)
+  }
+
+  last() {
+    if (!this.tail) return this
+    return this.tail.last()
+  }
+
   prepend(val) {
     const list = new List(val)
     list.tail = this
     return list
-  }
-
-  size() {
-    if (!this.tail) return 1
-    return 1 + this.tail.size()
   }
 
   toString() {
@@ -36,9 +57,79 @@ class List {
   }
 }
 
+function reverse(list) {
+  let a = list
+  let b = list.drop(1)
+  let c = list.drop(2)
+
+  a.tail = null
+
+  while (b) {
+    b.tail = a
+    a = b
+    b = c
+    c = c ? c.tail : null
+  }
+
+  return a
+}
+
+function reverseRec(list) {
+  function _reverse(l) {
+    if (!l.tail) {
+      list = l
+      return
+    }
+
+    _reverse(l.tail)
+
+    const n = l.tail
+
+    n.tail = l
+    l.tail = null
+  }
+
+  _reverse(list)
+
+  return list
+}
+
+function reverseTailRec(list) {
+  function inner(l, acc) {
+    if (!l) return acc
+
+    const tail = l.tail
+
+    l.tail = acc
+    return inner(tail, l)
+  }
+
+  return inner(list, null)
+}
+
+function reverseTailRecImmutable(list) {
+  function inner(list, acc) {
+    if (!acc) return inner(list.tail, new List(list.val))
+    if (!list) return acc
+    return inner(list.tail, acc.prepend(new List(list.val)))
+  }
+
+  return inner(list, null)
+}
+
+function reversePrint(list) {
+  if (!list) return
+  reversePrint(list.tail)
+  // eslint-disable-next-line no-console
+  console.log(list.val)
+}
+
 function middle(list) {
   const middle = Math.floor(list.size() / 2)
   return list.get(middle)
 }
 
-middle(List.fromArray([3,2,5,1,9]))
+const l = List.fromArray([7,8,9,13,2])
+
+reversePrint(l)
+middle(l)
